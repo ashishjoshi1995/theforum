51f0520384288faa7ff7c7ea81193a7ed7b0134f
deep-154 <deepankar11093@gmail.com>
2015-12-31 03:49:53 +0530

    styles created

---
+++ app/src/main/java/com/theforum/Constants.java
@@ -0,0 +1,10 @@
+package com.theforum;
+
+/**
+ * @author DEEPANKAR
+ * @since 31-12-2015.
+ */
+public class Constants {
+
+    public static final String USER_ID = "user_id";
+}
---
+++ app/src/main/java/com/theforum/TheForumApplication.java
@@ -0,0 +1,10 @@
+package com.theforum;
+
+import android.app.Application;
+
+/**
+ * @author DEEPANKAR
+ * @since 31-12-2015.
+ */
+public class TheForumApplication extends Application {
+}
