2e3aefc55f2c4210c9507e55a5385d637b0672e4
deep-154 <deepankar11093@gmail.com>
2016-01-05 03:15:33 +0530

    profile updated to design

--- app/src/main/java/com/theforum/home/ProfileFragment.java
+++ app/src/main/java/com/theforum/home/ProfileFragment.java
@@ -1,13 +1,18 @@
 package com.theforum.home;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.theforum.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -15,6 +20,14 @@ import butterknife.ButterKnife;
  * @since 31-12-2015.
  */
 public class ProfileFragment extends Fragment {
+
+    @Bind(R.id.profile_status) EditText status;
+    @Bind(R.id.profile_points) EditText points;
+    @Bind(R.id.profile_topics) EditText topics;
+
+    @Bind(R.id.profile_status_icon) ImageView statusIcon;
+    @Bind(R.id.profile_points_icon) ImageView pointsIcon;
+    @Bind(R.id.profile_topics_icon) ImageView topicsIcon;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_profile, container, false);
@@ -25,6 +38,17 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
+        status.setText("Smart");
+        points.setText("930");
+        topics.setText("100");
 
+        setBackgroundColor(statusIcon,"#313c44");
+        setBackgroundColor(pointsIcon,"#d9ab1d");
+        setBackgroundColor(topicsIcon,"#643173");
+
+    }
+
+    private void setBackgroundColor(ImageView imageView,String hexColor){
+        imageView.getBackground().setColorFilter(Color.parseColor(hexColor), PorterDuff.Mode.SRC_ATOP);
     }
 }
