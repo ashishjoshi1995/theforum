cab10c50f545f046bafa376ee0979262dfc9546f
Ashish Joshi <ashish1995joshi@gmail.com>
2016-01-09 18:25:18 +0530

    fab colour changed

---
+++ app/src/main/java/com/theforum/data/helpers/upvoteDownvoteApi/UPDVRequest.java
@@ -0,0 +1,12 @@
+package com.theforum.data.helpers.upvoteDownvoteApi;
+
+/**
+ * @author Ashish
+ * @since 1/9/2016
+ */
+public class UPDVRequest {
+    public String opinion_id;
+    public String opinion_owner_id;
+    public int operation_chosen;
+}
+
---
+++ app/src/main/java/com/theforum/data/helpers/upvoteDownvoteApi/UPDVResponse.java
@@ -0,0 +1,9 @@
+package com.theforum.data.helpers.upvoteDownvoteApi;
+
+/**
+ * @author Ashish
+ * @since 1/9/2016
+ */
+public class UPDVResponse {
+    public String message;
+}
