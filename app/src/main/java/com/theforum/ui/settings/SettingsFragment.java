8b06ae98d3689d758f1ff77f78cf4299fb4eb06a
Ashish Joshi <ashish1995joshi@gmail.com>
2016-01-29 02:09:13 +0530

    settings modified

--- app/src/main/java/com/theforum/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/theforum/ui/settings/SettingsFragment.java
@@ -11,7 +11,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.theforum.R;
 import com.theforum.TheForumApplication;
@@ -25,16 +28,23 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class SettingsFragment extends Fragment implements OnListItemClickListener{
+public class SettingsFragment extends Fragment implements View.OnClickListener,CompoundButton.OnCheckedChangeListener {
 
-    @Bind(R.id.toolbar)
-    Toolbar mToolbar;
-
-    @Bind(R.id.settings_recycler_view)
-    RecyclerView mRecyclerView;
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.settings_layout_application_share_the_app) TextView shareTheApp;
+    @Bind(R.id.settings_layout_legal_copyrights) TextView copyRights;
+    @Bind(R.id.settings_layout_legal_privacy_policy) TextView privacyPolicy;
+    @Bind(R.id.settings_layout_legal_terms_of_service) TextView termsOfService;
+    @Bind(R.id.settings_layout_application_feedback) TextView applicationFeedback;
+    @Bind(R.id.settings_layout_application_rate_us) TextView rateUs;
+    @Bind(R.id.settings_layout_application_contact_us) TextView contactUs;
+    @Bind(R.id.settings_layout_notification_opinions_received) CheckBox opinionsReceived;
+    @Bind(R.id.settings_layout_notification_renewal_requests) CheckBox renewalRequest;
+    @Bind(R.id.settings_layout_notification_topic_renewed) CheckBox topicRenewed;
+    @Bind(R.id.settings_layout_notification_upvotes_received) CheckBox upvotesReceived;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.settings_layout_temp, container, false);
     }
 
     @Override
@@ -53,57 +63,64 @@ public class SettingsFragment extends Fragment implements OnListItemClickListene
             }
         });
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, (int)CommonUtils.convertDpToPixel(12, getContext()), 0, 0);
-        mRecyclerView.setLayoutParams(params);
+        shareTheApp.setOnClickListener(this);
+        copyRights.setOnClickListener(this);
+        privacyPolicy.setOnClickListener(this);
+        termsOfService.setOnClickListener(this);
+        applicationFeedback.setOnClickListener(this);
+        rateUs.setOnClickListener(this);
+        contactUs.setOnClickListener(this);
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-        mRecyclerView.addItemDecoration(new DividerItemDecorator(getActivity(), R.drawable.recycler_view_divider));
-        mRecyclerView.setAdapter(new SettingsListAdapter(this, getListData()));
+        opinionsReceived.setOnCheckedChangeListener(this);
+        renewalRequest.setOnCheckedChangeListener(this);
+        topicRenewed.setOnCheckedChangeListener(this);
+        upvotesReceived.setOnCheckedChangeListener(this);
 
-    }
 
-    private ArrayList<String> getListData(){
-        ArrayList<String> data = new ArrayList<>();
-        data.add("Spread The World");
-        data.add("Share the App");
-        data.add("App");
-        data.add("Rate Us");
-        data.add("Feedback");
-        data.add("Reach Us");
-        data.add("Support");
-        data.add("Contact Us");
-        data.add("Legal");
-        data.add("Terms Of Service");
-
-        return data;
     }
 
     @Override
-    public void onItemClick(View v, int position) {
-        switch (position){
-            case 1:
-                CommonUtils.shareViaWatsapp(getActivity(),"Try this app theforum,\nregister as a tester on\nhttps://play.google.com/apps/testing/com.theforum " +
-                        "\nThen download it from playstore link on the page that follows.\n" +
-                        "For more details visit\nhttp://theforumapp.co/terms.html");
+    public void onClick(View v) {
+
+            switch(v.getId()){
+                case R.id.settings_layout_application_share_the_app:
+                    CommonUtils.shareViaWatsapp(getActivity(),"Try this app theforum,\nregister as a tester on\nhttps://play.google.com/apps/testing/com.theforum " +
+                            "\nThen download it from playstore link on the page that follows.\n" +
+                            "For more details visit\nhttp://theforumapp.co/terms.html");
+                    break;
+                case R.id.settings_layout_application_rate_us:
+                    CommonUtils.goToUrl(getActivity(),"https://play.google.com/store/apps/details?id=com.theforum");
+                    break;
+                case R.id.settings_layout_application_feedback:
+                    CommonUtils.emailIntent(getActivity());
+                    break;
+                case R.id.settings_layout_application_contact_us:
+                    CommonUtils.emailIntent(getActivity());
+                    break;
+                case R.id.settings_layout_legal_terms_of_service:
+                    CommonUtils.goToUrl(getActivity(),"http://theforumapp.co/terms.html");
+                    break;
+                case R.id.settings_layout_legal_privacy_policy:
+                    CommonUtils.goToUrl(getActivity(),"http://theforumapp.co/privacy.html");
+                    break;
+                case R.id.settings_layout_legal_copyrights:
+                    //CommonUtils.showToast(getActivity());
+                    break;
+            }
+        }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.settings_layout_notification_upvotes_received:
                 break;
-            case 3 :
-                CommonUtils.goToUrl(getActivity(),"https://play.google.com/store/apps/details?id=com.theforum&ah=DXRxB5Fc4Rba7GboG-pIzyRoJRI");
+            case R.id.settings_layout_notification_renewal_requests:
                 break;
-            case 4 :
-                CommonUtils.emailIntent(getActivity());
+            case R.id.settings_layout_notification_topic_renewed:
                 break;
-            case 6:
-                CommonUtils.emailIntent(getActivity());
-                break;
-            case 7:
-                CommonUtils.emailIntent(getActivity());
-                break;
-            case 9:
-                CommonUtils.goToUrl(getActivity(),"http://theforumapp.co/index.html");
+            case R.id.settings_layout_notification_opinions_received:
                 break;
         }
     }
-
 }
+
